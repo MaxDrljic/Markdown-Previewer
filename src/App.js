1b50bb4003c0dbe2b1e891f9a59ab3c2bccf033a
MaxDrljic <max_spree@hotmail.com>
2018-03-01 10:14:36 +0100

    Created Markdown Previewer

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import "./App.css";
+import FormGroup from "react-bootstrap/lib/FormGroup";
+import ControlLabel from "react-bootstrap/lib/ControlLabel";
+import FormControl from "react-bootstrap/lib/FormControl";
+
+const marked = require("marked");
+
+class App extends Component {
+
+  state = {
+    markdown: ""
+  };
+
+  updateMarkdown = function(markdown) {
+    this.setState({ markdown });
+  };
+
+  render() {
+    let { markdown } = this.state;
+
+    return (
+      <div className="App container">
+        <div>
+          <FormGroup controlId="formControlsTextarea">
+            <ControlLabel>Markdown Input</ControlLabel>
+            <FormControl
+              componentClass="textarea"
+              placeholder="Enter Markdown"
+              value={markdown}
+              onChange={event => this.updateMarkdown(event.target.value)}
+            />
+          </FormGroup>
+        </div>
+        <div>
+          <h1>Markdown Output</h1>
+          <div dangerouslySetInnerHTML={{ __html: marked(markdown) }} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
